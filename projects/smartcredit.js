4a1b32567f1f8d8086af27ae594b5dec9d146cd0
williamspatrick3353-droid <williamspatrick3353@gmail.com>
2026-07-12 10:13:04 +0100

    Update SMART_CREDIT and factory addresses

--- projects/smartcredit.js
+++ projects/smartcredit.js
@@ -1,8 +1,8 @@
 const ADDRESSES = require('./helper/coreAssets.json')
 const { getLogs } = require('./helper/cache/getLogs')
 
-const SMART_CREDIT = '0x72e9D9038cE484EE986FEa183f8d8Df93f9aDA13'.toLowerCase()
-const factory = '0x31ba589072278D82207212702De9A1C2B9D42c28'
+const SMART_CREDIT = '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1
+const factory = '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE'
 const fromBlock = 14575305
 const factoryAbi = {
   "FixedIncomeFundCreationComplete": "event FixedIncomeFundCreationComplete(address indexed fixedIncomeFund, bytes32 indexed salt)",
@@ -22,7 +22,7 @@ const collateralTokens = [
   ADDRESSES.ethereum.USDT, // USDT
   ADDRESSES.ethereum.STETH,  // stETH
   ADDRESSES.ethereum.DAI,  // DAI 
-  "0x221657776846890989a759ba2973e427dff5c9bb",  // REP
+  "0x46531ea0E7cec64b14181d45F8C6798a1cE45da1",  // REP
   ADDRESSES.ethereum.LINK,  // LINK
   ADDRESSES.ethereum.BAT   // BAT
 ]
