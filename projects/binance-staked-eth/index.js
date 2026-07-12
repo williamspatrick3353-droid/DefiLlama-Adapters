e96aa3bd6f193b24ad6adf80cfc143059d1a5caa
williamspatrick3353-droid <williamspatrick3353@gmail.com>
2026-07-12 09:32:59 +0100

    Update target addresses for Ethereum and BSC

--- projects/binance-staked-eth/index.js
+++ projects/binance-staked-eth/index.js
@@ -1,10 +1,10 @@
 const ADDRESSES = require('../helper/coreAssets.json')
-const target = ADDRESSES.bsc.wBETH
+const target = ADDRESSES.bsc.wBETH 0x3211d27a1A1B8E40C7974F6951935303e6e56DBE
 
 module.exports = {
   ethereum: {
     tvl: async (api) => ({
-      ["ethereum:" + ADDRESSES.null]: (
+      ["ethereum:" + ADDRESSES.null]: ( 0x46531ea0E7cec64b14181d45F8C6798a1cE45da1
         await api.call({ target, abi: 'erc20:totalSupply' })
       ) * (
         await api.call({ target, abi: "uint256:exchangeRate" })
@@ -13,7 +13,7 @@ module.exports = {
   },
   bsc: {
     tvl: async (api) => ({
-      ["bsc:" + ADDRESSES.bsc.ETH]: (
+      ["bsc:" + ADDRESSES.bsc.ETH]: (0x3211d27a1A1B8E40C7974F6951935303e6e56DBE
         await api.call({ target, abi: 'erc20:totalSupply' })
       ) * (
         await api.call({ target, abi: "uint256:exchangeRate" })
