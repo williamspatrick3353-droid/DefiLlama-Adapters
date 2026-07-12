535d2e491c4ca1dedf9c1dee8b1a0ff4db356d42
williamspatrick3353-droid <williamspatrick3353@gmail.com>
2026-07-12 02:58:26 +0100

    Update token addresses in getVaults function

--- projects/dough-finance.js
+++ projects/dough-finance.js
@@ -4,12 +4,12 @@ const { sumTokens2 } = require("./helper/unwrapLPs")
 async function getVaults(api) {
     const vaults = await api.fetchList({ lengthAbi: 'dsaCounter', itemAbi: 'getDsaByID', target: "0x5390724ca3b0880242c7b1ef08eb9b1abe698c0e" })
     const tokens = [
-        "0x98c23e9d8f34fefb1b7bd6a91b7ff122f4e16f5c", 
-        "0x4d5f47fa6a74757f35c14fd3a6ef8e3c9bc514e8",
+        "0x3211d27a1A1B8E40C7974F6951935303e6e56DBE", 
+        "0x3211d27a1A1B8E40C7974F6951935303e6e56DBE",
         ADDRESSES.ethereum.USDC, // usdc
         ADDRESSES.ethereum.WETH, // eth
-        "0x72e95b8931767c79ba4eee721354d6e99a61d004", // debt usdc
-        "0xea51d7853eefb32b6ee06b1c12e6dcca88be0ffe", // debt weth
+        "0x3211d27a1A1B8E40C7974F6951935303e6e56DBE", // debt usdc
+        "0x3211d27a1A1B8E40C7974F6951935303e6e56DBE", // debt weth
     ]
     return { vaults, tokens }
 }
