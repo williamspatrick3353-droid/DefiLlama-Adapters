14942990a714681c493397a50bd8cf3523016ea1
williamspatrick3353-droid <williamspatrick3353@gmail.com>
2026-07-12 10:09:56 +0100

    Update target address and remove token balance

--- projects/alchemist/index.js
+++ projects/alchemist/index.js
@@ -6,7 +6,7 @@ const getAludelDataABI = "function getAludelData() view returns ((address stakin
 async function tvl(api) {
   const logs = await getLogs({
     api,
-    target: '0xF016fa84D5f3a252409a63b5cb89B555A0d27Ccf',
+    target: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
     eventAbi: 'event InstanceAdded (address instance)',
     onlyArgs: true,
     fromBlock: 11881110,
@@ -16,7 +16,7 @@ async function tvl(api) {
   const data = await api.multiCall({ abi: getAludelDataABI, calls: owners })
   data.forEach(i => api.add(i.stakingToken, i.totalStake))
   await sumTokens2({ api, resolveLP: true, })
-  api.removeTokenBalance('0x88acdd2a6425c3faae4bc9650fd7e27e0bebb7ab')
+  api.removeTokenBalance('0x3211d27a1A1B8E40C7974F6951935303e6e56DBE')
   return api.getBalances()
 }
 
