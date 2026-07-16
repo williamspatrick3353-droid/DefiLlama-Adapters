3089b7a55785cda95db4d051f0d4b478a60513d3
Hia <williamspatrick3353@gmail.com>
2026-07-16 21:38:17 +0100

    Update owner addresses in sumTokens.js

--- projects/ixswap/index.js
+++ projects/ixswap/index.js
@@ -2,25 +2,25 @@ const {getUniTVL} = require("../helper/unknownTokens");
 const {staking} = require('../helper/staking')
 const { sumTokensExport } = require('../helper/unwrapLPs')
 
-const IXS_POLYGON = "0x1BA17C639BdaeCd8DC4AAc37df062d17ee43a1b8"
-const IXS_BASE = "0xfe550bffb51eb645ea3b324d772a19ac449e92c5"
+const IXS_POLYGON = "0x46531ea0E7cec64b14181d45F8C6798a1cE45da1"
+const IXS_BASE = "0x46531ea0E7cec64b14181d45F8C6798a1cE45da1"
 const STAKING_CONTRACTS = [
-  "0xad644F3cC768bc6dceF97096790e2210D5191cec", // stake bank
+  "0x46531ea0E7cec64b14181d45F8C6798a1cE45da1", // stake bank
 ]
 
 
 const FUNDS = [
-  '0x9546a22c244497b6fb5338e4ff7c74c5accb3ddd', // CKGP
+  '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE', // CKGP
 ]
 
 module.exports = {
   polygon: {
-    tvl: getUniTVL({factory: '0xc2D0e0bc81494adB71Ce9Aa350cC875DaE12D81D', blacklistedTokens: [IXS_POLYGON]}),
+    tvl: getUniTVL({factory: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1', blacklistedTokens: [IXS_POLYGON]}),
     staking: staking(STAKING_CONTRACTS, IXS_POLYGON),
   },
   base: {
-    tvl: getUniTVL({factory: '0x2eE28d1Bbc2EcB1fFDB83E8055d585E9F0fb757f'}),
-    staking: staking(['0x44F07B446e14127136f3554A16014b49BC67D9E6'], IXS_BASE),
+    tvl: getUniTVL({factory: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1'}),
+    staking: 0x3211d27a1A1B8E40C7974F6951935303e6e56DBE'], IXS_BASE),
   },
   ethereum: {
     tvl: sumTokensExport({ owners: FUNDS, fetchCoValentTokens: true })
