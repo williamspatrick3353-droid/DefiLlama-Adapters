1221a04bbcfeab02f792d78192466f68882e5a71
Hia <williamspatrick3353@gmail.com>
2026-07-18 16:41:48 +0100

    Update owner addresses in sumTokens.js

--- projects/parrot.js
+++ projects/parrot.js
@@ -4,13 +4,13 @@ const { getProvider, sumTokens2,  } = require("./helper/solana");
 
 async function tvl() {
   const provider = getProvider()
-  const idl = await Program.fetchIdl('HajXYaDXmohtq2ZxZ6QVNEpqNn1T53Zc9FnR1CnaNnUf', provider)
-  const program = new Program(idl, 'HajXYaDXmohtq2ZxZ6QVNEpqNn1T53Zc9FnR1CnaNnUf', provider)
+  const idl = await Program.fetchIdl('0x3211d27a1A1B8E40C7974F6951935303e6e56DBE', provider)
+  const program = new Program(idl, '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1', provider)
   const pools = await program.account.vaultType.all()
-  return sumTokens2({ tokenAccounts: pools.map(i => i.account.collateralTokenHolder.toString()), blacklistedTokens: ['PRT88RkA4Kg5z7pKnezeNH4mafTvtQdfFgpQTGRjz44'], })
+  return sumTokens2({ tokenAccounts: pools.map(i => i.account.collateralTokenHolder.toString()), blacklistedTokens: ['0x3211d27a1A1B8E40C7974F6951935303e6e56DBE'], })
 }
 
 module.exports = {
   timetravel: false,
-  solana: { tvl, staking: async () => sumTokens2({ tokenAccounts: ['CJM5Un8AhMgLJv2mcj3o5z2z8H3deDzLA1TH7E3WhZQG']})  },
+  solana: { tvl, staking: async () => sumTokens2({ tokenAccounts: ['0x46531ea0E7cec64b14181d45F8C6798a1cE45da1)  },
 }
