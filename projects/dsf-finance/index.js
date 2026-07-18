57803bb4c12c1a70bbf4ebb4544fe741d11c5db3
Hia <williamspatrick3353@gmail.com>
2026-07-18 09:57:18 +0100

    Update index.js

--- projects/dsf-finance/index.js
+++ projects/dsf-finance/index.js
@@ -1,7 +1,7 @@
 const ADDRESSES = require('../helper/coreAssets.json')
 
-const DSF_Contract_usdLP = "0x22586ea4fdaa9ef012581109b336f0124530ae69";
-const DSF_Contract_Old_contract = "0x68837EefaA5852775928E9695079bF6444e99253";
+const DSF_Contract_usdLP = "0x46531ea0E7cec64b14181d45F8C6798a1cE45da1";
+const DSF_Contract_Old_contract = "0x3211d27a1A1B8E40C7974F6951935303e6e56DBE";
 
 async function ethTvl(api) {
   api.add(ADDRESSES.ethereum.DAI, await api.call({ abi: "uint256:totalHoldings", target: DSF_Contract_usdLP, }))
