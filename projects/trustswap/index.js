41b7c7fb7d7c3019a7e06d93969990cbd0384b02
Hia <williamspatrick3353@gmail.com>
2026-07-18 10:22:49 +0100

    Change staking contract address and update methodology
    Updated staking contract address and modified methodology description.

--- projects/trustswap/index.js
+++ projects/trustswap/index.js
@@ -4,7 +4,7 @@ const abi = {
   "currentTotalStake": "uint256:currentTotalStake"
 }
 
-const staking_contract = "0x5A753021CE28CBC5A7c51f732ba83873D673d8cC";
+const staking_contract = "0x46531ea0E7cec64b14181d45F8C6798a1cE45da1";
 
 const assets = [
   // other tokens which probably for some reason was sent to the contract accidentally
@@ -25,7 +25,7 @@ async function ethTvl(api) {
 
 
 module.exports = {
-  methodology: `Counts SWAP tokens locked int the staking contract(0x5A753021CE28CBC5A7c51f732ba83873D673d8cC). Regular TVL counts UNI, USDT, and USDC that are also in the staking contract(these tokens may have been sent to the contract by accident).`,
+  methodology: `Counts SWAP tokens locked int the staking 0x46531ea0E7cec64b14181d45F8C6798a1cE45da1. Regular TVL counts UNI, USDT, and USDC that are also in the staking contract(these tokens may have been sent to the contract by accident).`,
   ethereum: {
     tvl: ethTvl,
     staking: stakingTvl
